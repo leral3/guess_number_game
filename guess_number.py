bc9cce3067e87b473e79ddf75695f2d1abbdcecf
leral3 <leral3@naver.com>
2022-03-11 05:39:28 +0900

    판단하기

--- guess_number.py
+++ guess_number.py
@@ -1,2 +1,8 @@
-guess = input("숫자를 입력하세요:")
-print(guess)
+number = 35
+
+guess = int(input("숫자를 입력하세요:"))   # input으로 입력받은 숫자의 데이터 타입은 문자열이므로 이를 정수형으로 변환해야함
+
+if guess == number:
+    print("정답입니다.")
+else:
+    print("오답입니다.")
