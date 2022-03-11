fd7bfb27f3d3a48ba85c95a1a3679223843421a9
leral3 <leral3@naver.com>
2022-03-12 03:41:52 +0900

    추가 조건 부여, 횟수지정

--- guess_number.py
+++ guess_number.py
@@ -3,11 +3,16 @@ import random
 number = random.randint(1,999)                # 정답을 랜덤으로 생성하기 
 print(number)
 
-
-while True:
+chance = 0
+while True:    
+    if chance == 5:
+        print("모든 시도가 끝났습니다.")
+        break
+    chance += 1
+    
     try:
         guess = int(input("숫자를 입력하세요:"))   # input으로 입력받은 숫자의 데이터 타입은 문자열이므로 이를 정수형으로 변환해야함
-        
+
         if guess == number:
             print("정답입니다.")
             break                                 # 정답이면, 루프 문을 빠져 나갑니다.   
@@ -16,5 +21,6 @@ while True:
             print("입력하신 값이 더 큽니다.")
         else:
             print("입력하신 값이 더 작습니다.")
+
     except:
         print("1~999 사이의 숫자를 입력하세요")
