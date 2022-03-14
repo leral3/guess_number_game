0416b39d45d8b1faf80bc945311fe29dc327d09a
leral3 <leral3@naver.com>
2022-03-15 00:54:40 +0900

    힌트기능추가 각자리수 더한값

--- guess_number.py
+++ guess_number.py
@@ -4,12 +4,20 @@ number = random.randint(1,999)                # 정답을 랜덤으로 생성하
 print(number)
 
 def number_hint():
-    print("랜덤으로 생선된 숫자에 대한 힌트를 알려드립니다.")
-    if number % 2 ==0:
-        print("해당 숫자는 짝수 입니다.")
-    else:
-        print("해당 숫자는 홀수 입니다.")
+    print("랜덤으로 생성된 숫자에 대한 힌트는 총 2가지입니다. 힌트를 확인하시려면 1번 또는 2번을 눌러주세요")
 
+    a = int(input(""))
+    if a == 1:
+        if number % 2 == 0:
+            print("해당 숫자는 짝수 입니다.")
+        else:
+            print("해당 숫자는 홀수 입니다.")
+    
+    elif a == 2:
+        print("각 자리수를 더한 값은 {} 입니다".format(sum(map(int,str(number)))))
+
+    elif a == 3:
+        print("에휴...")
 
 chance = 0
 
@@ -23,12 +31,9 @@ while True:
             print("입력하신 값이 더 큽니다.")
         elif guess < number:
             print("입력하신 값이 더 작습니다.")
-
-        print("힌트를 알고 싶으신가요?")
         
-        hint = int(input("힌트를 알고 싶으시면 0번을 누르세요"))
-
-        if hint == 0 :
+        hint = 0
+        if hint == int(input("힌트를 알고 싶으시면 0번을 누르세요")) :
             number_hint()
 
         if chance == 3:
